81bf7c4092ef75b80c1e66ec3dda1cdf3605da6f
lpavezleon@gmail.com <Azulado710>
2021-11-23 20:47:21 -0300

    API Rest, supermarket platzi, antes de pasar a pdn

---
+++ src/main/java/com/platzi/market/web/security/SecurityConfig.java
@@ -0,0 +1,68 @@
+package com.platzi.market.web.security;
+
+import com.platzi.market.domain.service.PlatziUserDetailsService;
+import com.platzi.market.web.security.filter.JwtFilterRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PlatziUserDetailsService platziUserDetailsService;
+
+    @Autowired
+    private JwtFilterRequest jwtFilterRequest;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+      auth.userDetailsService(platziUserDetailsService);
+    }
+
+    @Override
+    protected  void configure(HttpSecurity http) throws Exception {
+       http.csrf().disable()
+               .authorizeRequests().antMatchers("/**/authenticate").permitAll()
+               .anyRequest().authenticated().and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+       http.addFilterBefore(jwtFilterRequest, UsernamePasswordAuthenticationFilter.class);
+    }
+
+    @Override
+    @Bean
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
+    }
+
+    /*
+    Con la configuración de SecurityConfig se está denegando el acceso a las uris de swagger.
+    Con esta configuración extra en la clase SecurityConfig se puede habilitar el acceso.
+    */
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().antMatchers(
+                "/v2/api-docs/**",
+                "/configuration/ui",
+                "/swagger-resources/**",
+                "/configuration/security",
+                "/swagger-ui.html",
+                "/webjars/**");
+    }
+
+
+   // Otra alternativa es agregar estas rutas 👆 al
+    // antMatchers("/**/authenticate", …)
+    // que escribimos en el método configure(HttpSecurity http)
+    // de esta clase que estás viendo.
+
+
+}
